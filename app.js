acc729430fb1c28035593e93a46b1da1fbde7b10
Aakash Mahesha <aakash280500@gmail.com>
2023-08-04 17:59:33 -0400

    changed cors definition of urls

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ import "dotenv/config";
 
 const app = express();
 app.use(cors({
+    // {
     credentials: true,
-    origin: process.env.FRONTEND_URL,
-},
-{
-    credentials: true,
-    origin: process.env.FRONTEND_DEPLOY_URL,
+    origin: [process.env.FRONTEND_URL,process.env.FRONTEND_DEPLOY_URL]
+    // },
+//     {
+//     credentials: true,
+//     origin: process.env.FRONTEND_DEPLOY_URL,
+// },
 }));
 
 const sessionOptions = {
