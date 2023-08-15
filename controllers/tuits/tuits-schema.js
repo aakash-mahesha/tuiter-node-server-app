c5a018c59faa36215f626681d2675f1ac602095f
Aakash Mahesha <aakash280500@gmail.com>
2023-08-15 15:50:33 -0400

    modified tuit schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
-  tuit: String,
-  likes: Number,
-  liked: Boolean,
-  image: String,
+  tuit: {type: String},
+  likes: {type: Number},
+  liked: {type: Boolean},
+  image: {type: String},
+  username: { type: String, default: 'Nasa'},
+  handle: { type: String, default: '@nasa'},
+  title: {type: String},
+  dislikes: {type: Number, default:0},
+  replies: {type: Number, default:0},
+  time: { type: String, default:'5h'},
+  retuits: {type: Number, default: 0}
 }, {collection: 'tuits'});
 export default schema;
 
